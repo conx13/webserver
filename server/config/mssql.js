15808639720a7af3569a33e59af481a868ba2a17
conx13 <hillar@gmail.com>
2022-08-03 17:54:08 +0300

    edasi

--- server/config/mssql.js
+++ server/config/mssql.js
@@ -1,10 +1,5 @@
 module.exports = require('knex')({
   client: 'mssql',
-<<<<<<< HEAD
-  // Kui on vaja logida sql päringut:
-  //debug: true,
-=======
->>>>>>> 46248cb4e12babb92ef80d11bab89b4d14137dc1
     connection: {
         user: 'Hillar',
         password: 'conx13',
