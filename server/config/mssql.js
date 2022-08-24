5538ce2f1e3533d5b153ddde85883418c7851ad3
conx13 <hillar@gmail.com>
2022-08-24 16:06:00 +0300

    edasi

--- server/config/mssql.js
+++ server/config/mssql.js
@@ -1,14 +1,14 @@
-module.exports = require('knex')({
-  client: 'mssql',
-    connection: {
-        user: 'Hillar',
-        password: 'conx13',
-        database: 'Ribakood',
-        server: '10.0.30.2',
-        pool: {
-          max: 10,
-          min: 0,
-          idleTimeoutMillis: 30000
-        },
+module.exports = require("knex")({
+  client: "mssql",
+  connection: {
+    user: "Hillar",
+    password: "conx13",
+    database: "Ribakood",
+    server: "10.0.30.2",
+    pool: {
+      max: 10,
+      min: 0,
+      idleTimeoutMillis: 30000,
     },
-})
+  },
+});
--- server/controllers/rkoodCont.js
+++ server/controllers/rkoodCont.js
@@ -122,7 +122,7 @@ const otsiTood = (req, res, next) => {
 const kesTegi = (req, res, next) => {
   knex("w_rk_kes_tegi")
     .where("jid", req.params.jid)
-    .orderBy([{ column: "PNIMI" }, { column: "start" }])
+    .orderBy([{ column: "nimi" }, { column: "start" }])
     .then((rows) => {
       res.status(200).json(rows);
     })
